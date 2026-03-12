e662d7be1270f6ca9ef1bc1f30c1f8cea780246c
mahdi-islam-pranto <mahdiprantoblog@gmail.com>
2026-03-12 14:42:49 +0600

    logger with logs file + all notebooks and dataset has been added

--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,11 @@
 import sys
+from src.logger import logging
 
 # This function is used to create a custom error message that includes the file name, line number, and error message
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
-     file_name,exc_tb.tb_lineno,str(error))
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
 
@@ -18,3 +18,13 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+
+# test custom exception
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         # put the error message in the log file
+#         logging.info("Divide by zero error occurred.")
+#         raise CustomException(e,sys)
